cb16ac189a869dc079d184154f606eae15c04284
Samy <samy.25dz@gmail.com>
2019-08-26 18:16:36 +0100

    updated the header

--- header.php
+++ header.php
@@ -1,21 +1,20 @@
 <header>
-        <div class="container">
-                <a class="logo" href="#"><img src="images/logo-white.png" alt="Logo"></a>
+    <div class="container-fluid">
+        <a class="logo ml-40" href="#"><img src="images/logo-white.png" alt="Logo"></a>
 
-                <div class="right-area">
-                        <h6><a class="plr-20 color-white btn-fill-primary" href="#">COMMANDEZ AU: +213 558 74 69 73</a></h6>
-                </div><!-- right-area -->
+        <div class="right-area mr-40">
+            <h6><a class="plr-20 mr-15 color-white btn-fill-primary" href="#">COMMANDER: +213 558 74 69 73</a><a class="plr-20 color-white btn-fill-primary" href="#">Commander en ligne</a></h6>
+        </div><!-- right-area -->
 
-                <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
+        <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
 
-                <ul class="main-menu font-mountainsre" id="main-menu">
-                        <li><a href="index.php">Accueil</a></li>
-                        <li><a href="02_about_us.php">A Propos de nous</a></li>
-                        <li><a href="03_menu.php">Menu</a></li>
-                        <li><a href="04_blog.php">//</a></li>
-                        <li><a href="05_contact.php">Contactez nous</a></li>
-                </ul>
+        <ul class="main-menu font-mountainsre" id="main-menu">
+            <li><a href="index_.html">ACCUEIL</a></li>
+            <li><a href="02_about_us.html">A PROPOS DE NOUS</a></li>
+            <li><a href="03_menu.html">MENU</a></li>
+            <li><a href="05_contact.html">CONTACTEZ NOUS</a></li>
+        </ul>
 
-                <div class="clearfix"></div>
-        </div><!-- container -->
+        <div class="clearfix"></div>
+    </div><!-- container -->
 </header>
