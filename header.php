9318cb14403c889ca840da0dda182b27b315f9c8
Samy <samy.25dz@gmail.com>
2019-08-26 18:19:51 +0100

    updated the header once more

--- header.php
+++ header.php
@@ -3,16 +3,19 @@
         <a class="logo ml-40" href="#"><img src="images/logo-white.png" alt="Logo"></a>
 
         <div class="right-area mr-40">
-            <h6><a class="plr-20 mr-15 color-white btn-fill-primary" href="#">COMMANDER: +213 558 74 69 73</a><a class="plr-20 color-white btn-fill-primary" href="#">Commander en ligne</a></h6>
+            <h6>
+                <a class="plr-20 mr-15 color-white btn-fill-primary" href="#">COMMANDER: +213 558 74 69 73</a>
+                <a class="plr-20 color-white btn-fill-primary" href="07_login.php">Commander en ligne</a>
+            </h6>
         </div><!-- right-area -->
 
         <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
 
         <ul class="main-menu font-mountainsre" id="main-menu">
-            <li><a href="index_.html">ACCUEIL</a></li>
-            <li><a href="02_about_us.html">A PROPOS DE NOUS</a></li>
-            <li><a href="03_menu.html">MENU</a></li>
-            <li><a href="05_contact.html">CONTACTEZ NOUS</a></li>
+            <li><a href="index.php">ACCUEIL</a></li>
+            <li><a href="02_about_us.php">A PROPOS DE NOUS</a></li>
+            <li><a href="03_menu.php">MENU</a></li>
+            <li><a href="05_contact.php">CONTACTEZ NOUS</a></li>
         </ul>
 
         <div class="clearfix"></div>
