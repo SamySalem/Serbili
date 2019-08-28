1ce1687ad20b5ff4599e52f67205db790a4a5582
Samy <samy.25dz@gmail.com>
2019-08-28 20:44:46 +0100

    added annuler-commande-serveur.html and menu-commande-emporte.html

---
+++ header-employe.php
@@ -0,0 +1,11 @@
+<header id="header-employe">
+    <div class="container">
+        <a class="logo ml-40" href="#"><img src="images/logo-white.png" alt="Logo"></a>
+
+        <div class="right-area mr-40">
+            <h6>
+                <a class="plr-20 mr-15 mb-20 color-white btn-fill-primary">Username</a>
+            </h6>
+        </div><!-- right-area -->
+    </div><!-- container -->
+</header>
