f0248eee8397974e0763e41ea8624ec260181217
Samy <samy.25dz@gmail.com>
2019-09-04 15:41:53 +0100

    fixed 9andahar

--- verifier_commande.php
+++ verifier_commande.php
@@ -1,6 +1,7 @@
+<!DOCTYPE HTML>
 <?php
-   ob_start();
-   session_start();
+ob_start();
+session_start();
 ?>
 <?php include 'ConnectDB.php'; ?>
 <html lang="en">
@@ -19,7 +20,7 @@
         <link href="plugin-frameworks/swiper.css" rel="stylesheet">
         <link href="fonts/ionicons.css" rel="stylesheet">
         <link href="common/styles.css" rel="stylesheet">
-        
+
         <!-- SCIPTS -->     
         <script src="plugin-frameworks/jquery-3.2.1.min.js"></script>
         <script src="plugin-frameworks/bootstrap.min.js"></script>
@@ -27,9 +28,9 @@
         <script src="common/scripts.js"></script>
 
     </head>
-    
-   
-        
+
+
+
     <body id="ver-com">
 
 
@@ -38,87 +39,87 @@
 
 
             <div class="wrapper">
-			<?php
-          
-			// Create connection
-			$instance = ConnectDb::getInstance();
-			$conn = $instance->getConnection();
+                <?php
 
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			}
-           
-			// récuprer la liste des commandes faites par internet et non verifier
-			$sql = "SELECT numero_commande,Heur,Adresse,Client_Login FROM Commande as c, Commande_Internet as ci where c.numero_commande=ci.Commande_numero_commande and ci.Verifie=0";
-			$result = $conn->query($sql);
-            
-			if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-            $numero_commande = $row["numero_commande"];
-            $heur = $row["Heur"];
-			$adresse = $row["Adresse"];
-			$login = $row["Client_Login"];
-			
-			//récuperer telephone du client internet
-			$sql = "SELECT Telephone FROM Client where Login=".$login;
-			$result2 = $conn->query($sql);
-			while($row = $result->fetch_assoc()) {
-			$telephone = $row["Client_Login"];	
-			}
-			
-			//afficher les info
-			echo "<div class='accordion'>".
-                    "<div class='panel modify'>".
-                        "<p><b>Username : </b>".$login."</p>".
-                        "<p><b>N° de téléphone : </b>0".$telephone."</p>".
-                        "<p><b>Adresse : </b>".$adresse."</p>".
-                        "<p><b>N° de commande : </b>".$numero_commande."</p>".
-                        "<p><b>Date : </b>".$heur."</p>".
-                    "</div>".
-                    "<div class='panel-content'>".
-                        "<table class='com-ver'>".
-                            "<tr>".
-                                "<th>Intitulé</th>".
-                                "<th>Prix unité (DA)</th>".
-                                "<th>Quantité</th>".
-                            "</tr>";
-			
-			
-			//recuprer les elements commandés
-			$sql = "SELECT Intitule,Prix,Quantite FROM Element_menu,Element_Commande where Commande_numero_commande=".$numero_commande." and Element_Commande.Element_menu_Intitule=Element_menu.Intitule";
-			$result3 = $conn->query($sql);
-			while($row = $result3->fetch_assoc()) {
-				//afficher les elements commandés
-				echo "<tr>".
-                                "<td>".$row["Intitule"]."</td>".
-                                "<td>".$row["Prix"]."</td>".
-                                "<td>".$row["Quantite"]."</td>".
-                            "</tr>";
-			}
-			echo "</table>".
-                        "<div class='dplay-tbl'>".
-                            "<div class='dplay-tbl-cell center-text'>".
-                                "<a class='p-10 mt-10 btn btn-success' href='#'><b>Accepter</b></a>".
-                                "<a class='p-10 mt-10 btn btn-danger' href='#'><b>Refuser</b></a>".
+                // Create connection
+                $instance = ConnectDb::getInstance();
+                $conn = $instance->getConnection();
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                // récuprer la liste des commandes faites par internet et non verifier
+                $sql = "SELECT numero_commande,Heur,Adresse,Client_Login FROM Commande as c, Commande_Internet as ci where c.numero_commande=ci.Commande_numero_commande and ci.Verifie=0";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        $numero_commande = $row["numero_commande"];
+                        $heur = $row["Heur"];
+                        $adresse = $row["Adresse"];
+                        $login = $row["Client_Login"];
+
+                        //récuperer telephone du client internet
+                        $sql = "SELECT Telephone FROM Client where Login=".$login;
+                        $result2 = $conn->query($sql);
+                        while($row = $result->fetch_assoc()) {
+                            $telephone = $row["Client_Login"];	
+                        }
+
+                        //afficher les info
+                        echo "<div class='accordion'>".
+                            "<div class='panel modify'>".
+                            "<p><b>Username : </b>".$login."</p>".
+                            "<p><b>N° de téléphone : </b>0".$telephone."</p>".
+                            "<p><b>Adresse : </b>".$adresse."</p>".
+                            "<p><b>N° de commande : </b>".$numero_commande."</p>".
+                            "<p><b>Date : </b>".$heur."</p>".
                             "</div>".
-                        "</div>".
-                    "</div>".
-                "</div>";
-            }	
-           
-            //$_SESSION['valid'] = true;
-            //$_SESSION['timeout'] = time();
-            //$row = $result->fetch_assoc();
-            //$_SESSION['username'] = $row["login"];
-			
-            
-        } else {
-            echo 'pas de commande a vérifier';
-        }
-            $conn->close();
-        
-        ?>
+                            "<div class='panel-content'>".
+                            "<table class='com-ver'>".
+                            "<tr>".
+                            "<th>Intitulé</th>".
+                            "<th>Prix unité (DA)</th>".
+                            "<th>Quantité</th>".
+                            "</tr>";
+
+
+                        //recuprer les elements commandés
+                        $sql = "SELECT Intitule,Prix,Quantite FROM Element_menu,Element_Commande where Commande_numero_commande=".$numero_commande." and Element_Commande.Element_menu_Intitule=Element_menu.Intitule";
+                        $result3 = $conn->query($sql);
+                        while($row = $result3->fetch_assoc()) {
+                            //afficher les elements commandés
+                            echo "<tr>".
+                                "<td>".$row["Intitule"]."</td>".
+                                "<td>".$row["Prix"]." DA</td>".
+                                "<td>".$row["Quantite"]."</td>".
+                                "</tr>";
+                        }
+                        echo "</table>".
+                            "<div class='dplay-tbl'>".
+                            "<div class='dplay-tbl-cell center-text'>".
+                            "<a class='p-10 mt-10 btn btn-success' href='#'><b>Accepter</b></a>".
+                            "<a class='p-10 mt-10 btn btn-danger' href='#'><b>Refuser</b></a>".
+                            "</div>".
+                            "</div>".
+                            "</div>".
+                            "</div>";
+                    }	
+
+                    //$_SESSION['valid'] = true;
+                    //$_SESSION['timeout'] = time();
+                    //$row = $result->fetch_assoc();
+                    //$_SESSION['username'] = $row["login"];
+
+
+                } else {
+                    echo 'pas de commande a vérifier';
+                }
+                $conn->close();
+
+                ?>
                 <div class="accordion">
                     <div class="panel modify">
                         <p><b>Username : </b>anisanis</p>
